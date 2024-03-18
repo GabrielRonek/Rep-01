86e01d0bdfac0e4b5436fab0b933db97adec1e6b
GabrielRonek <s24515@pjwstk.edu.pl>
2024-03-18 19:13:13 +0100

    Dodano liczenie średniej

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Zmiana");
-Console.WriteLine("Kolejna Zmiana");
-Console.WriteLine("Jeszcze Kolejna Zmiana");
+﻿using System;
+ 
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args){
+           
+            int[] a = [-12, 6, -99, 2, 14, -4, 2];
+            countAv(a);
+        }  
+        static void countAv(int[] b){
+            float sum = 0;
+            for(int a = 0; a < b.Length; a++){
+                sum += b[a];
+            }
+            Console.WriteLine("The average of the given array is: {0}", sum/b.Length);
+        
+        }
+    }
+}
